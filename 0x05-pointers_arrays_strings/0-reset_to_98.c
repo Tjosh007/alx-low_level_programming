4c108d9e494f015afaa86ceac68a946159fe9f57
Tjosh007 <tjosh0077@gmail.com>
2022-06-13 09:09:57 -0700

    commit for Task 0, 0-reset_to_98.c added...

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - updates the value *n points to.
- * @n: the number to modify.
+ *reset_to_98 - updates a integer value to 98
+ *@n: A pointer to an int that will be updated
+ *
+ *Return: void that means is correct
  */
 void reset_to_98(int *n)
 {
