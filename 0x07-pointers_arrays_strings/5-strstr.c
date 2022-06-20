3c3f7f1e0ee003b1548f1c2551cf9dc330aa40f3
Tjosh007 <tjosh0077@gmail.com>
2022-06-20 14:15:45 -0700

    commit for Task 5, 5-strstr.c added...

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,58 +1,26 @@
 #include "main.h"
 /**
-* _strstr - function locate
-* @haystack: pointer to char
-* @needle: pointer to char
-* Return: 0
-*/
-char *_strstr(char *haystack, char *needle)
+ * _strstr - first occurrence of the substring needle in the string haystack
+ * @haystack: main str to be examined
+ * @needle: searched in haystack
+ * Return: return 0
+ **/
+char  *_strstr(char *haystack, char *needle)
 {
-char *result = haystack, *fneedle = needle;
-while (*haystack)
+char *str1, *str2; /*Declaring variables*/
+while (*haystack != '\0')
 {
-while (*needle)
+str1 = haystack; /*values*/
+str2 = needle;
+/*Star WHILE*/
+while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 {
-if (*haystack++ != *needle++)
-{
-break;
+haystack++;
+str2++;
 }
-}
-if (!*needle)
-{
-return (result);
-}
-needle = fneedle;
-result++;
-haystack = result;
-}
-return (0);
-}
-#include "main.h"
-/**
-* _strstr - function locate
-* @haystack: pointer to char
-* @needle: pointer to char
-* Return: 0
-*/
-char *_strstr(char *haystack, char *needle)
-{
-char *result = haystack, *fneedle = needle;
-while (*haystack)
-{
-while (*needle)
-{
-if (*haystack++ != *needle++)
-{
-break;
-}
-}
-if (!*needle)
-{
-return (result);
-}
-needle = fneedle;
-result++;
-haystack = result;
+if (*str2 == '\0')
+return (str1);
+haystack = str1 + 1;
 }
 return (0);
 }
